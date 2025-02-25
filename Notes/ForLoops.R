f9636ac368b2c5cb7530df2fd4c7365fad4516c9
SageSularz <sage.sularz@gmail.com>
2025-02-25 14:20:54 -0500

    Feb 15 Class notes

--- Notes/ForLoops.R
+++ Notes/ForLoops.R
@@ -178,11 +178,12 @@ head(species_area_curve())
 #        z is slope constant
 # Output: smoothed curve with perameter in graph
 #############################################################
-species_area_plot <- function(A = 1:5000, c = 0.5, z = 0.26) {
-  plot(x+A,y=species_area_curve(A,c,z),type="l", xlab="Island Area", ylab="S",ylim=c(0,2500))
-  mtext(paste("c =" c, "z =" z))
-  return(S)
+species_area_plot <- function(A=1:5000,c= 0.5,z=0.26) {
+  plot(x=A,y=species_area_curve(A,c,z),type="l",xlab="Island Area",ylab="S",ylim=c(0,2500))
+  mtext(paste("c =", c,"  z =",z),cex=0.7)
+  #      return()
 }
+species_area_plot()
 #############################################################
 
 head(species_area_curve())
@@ -196,6 +197,138 @@ for (i in seq_along(c_pars)) {
     species_area_plot(c=c_pars[i], z=z_pars[j])
   }
 }
+
+par(mfrow=c(1,1))
+
+#############################################################
+# Feb 25 2025
+#############################################################
+
+# Looping with while or repeat
+
+# looping with for
+# best way to create an op that you dont know what the end will be
+cut_point <- 0.1
+z <- NA
+ran_data <- runif(100)
+for (i in seq_along(ran_data)) {
+  z <- ran_data[i]
+  if (z < cut_point) break
+}
+print(z)
+print(i)
+
+
+# looping with while
+#
+z <- NA
+cycle_number <- 0
+while (is.na(z) | z >= cut_point) { # While the conditions are met....
+  z <- runif(1)
+  cycle_number <- cycle_number + 1
+
+}
+print(z)
+print(cycle_number)
+
+# Using the expand.grid() function to create a dataframe with parameter combinations
+expand.grid(c_pars, z_pars)
+# set up two variables (c_pars and z_pars) and paired up all the unique combos of the paramerters
+
+df <- expand.grid(c_pars=c_pars, z_pars=z_pars)
+head(df)
+str(df) # dataframe
+
+
+##################################################
+# function: sa_output
+# Summary stats for species-area power function
+# input: vector of predicted species richness
+# output: list of max-min, coefficient of variation
+##################################################
+sa_output <- function(S=runif(1:10)) {
+
+  sum_stats <- list(s_gain=max(S)-min(S),s_cv=sd(S)/mean(S))
+
+  return(sum_stats)
+}
+sa_output()
+
+# Build program body with a single loop through
+# the parameters in modelFrame
+
+# Global variables
+area <- 1:5000
+c_pars <- c(100,150,175)
+z_pars <- c(0.10, 0.16, 0.26, 0.3)
+
+# set up model frame
+model_frame <- expand.grid(c=c_pars,z=z_pars)
+model_frame$s_gain <- NA
+model_frame$s_cv <- NA
+print(model_frame)
+
+# cycle through model calculations
+for (i in 1:nrow(model_frame)) {
+
+  # generate S vector
+  temp1 <- species_area_curve(A=area,
+                              c=model_frame[i,1],
+                              z=model_frame[i,2])
+  # calculate output stats
+  temp2 <- sa_output(temp1)
+  # pass results to columns in data frame
+  model_frame[i,c(3,4)] <- temp2
+
+}
+print(model_frame)
+
+# Parameter sweeping redux with ggplot graphics
+library(ggplot2)
+
+area <- 1:5 #keep this very small initially
+c_pars <- c(100,150,175)
+z_pars <- c(0.10, 0.16, 0.26, 0.3)
+
+# set up model frame
+model_frame <- expand.grid(c=c_pars,z=z_pars,A=area)
+model_frame$S <- NA # set as NA for now and then use op to fill in
+
+# Multiple ways to approch this
+
+# first way more complex double for loop
+# cycles over the parameters
+# hard to read
+for (i in 1:length(c_pars)) {
+  for (j in 1:length(z_pars)) {
+    model_frame[model_frame$c==c_pars[i] & model_frame$z==z_pars[j],"S"] <- species_area_curve(A=area, c=c_pars[i], z=z_pars[j])
+  }
+}
+
+# second more straight forward option
+# for loop cycles over model frame structure
+for (i in 1:nrow(model_frame)) {
+  model_frame[i,"S"] <- species_area_curve(A=model_frame$A[i],
+                                           c=model_frame$c[i],
+                                           z=model_frame$z[i])
+}
+
+head(model_frame)
+
+p1 <- ggplot(data=model_frame)
+p1 + geom_line(mapping=aes(x=A,y=S)) +
+  facet_grid(c~z)
+
+
+p2 <- p1
+p2 + geom_line(mapping=aes(x=A,y=S,group=z)) +
+  facet_grid(.~c)
+
+
+p3 <- p1
+p3 + geom_line(mapping=aes(x=A,y=S,group=c)) +
+  facet_grid(z~.)
+
 
 
 
