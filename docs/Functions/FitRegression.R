86c26bc377f4dc26c6219772a21ec907c5557679
SageSularz <sage.sularz@gmail.com>
2025-03-26 13:14:48 -0400

    ....

---
+++ docs/Functions/FitRegression.R
@@ -0,0 +1,26 @@
+# --------------------------------------
+# FUNCTION fit_regression
+# required packages: none
+# description:
+# inputs:
+# outputs:
+########################################
+fit_regression <- function(x=NULL,y=NULL){
+
+# assign parameter defaults
+if (is.null(x) | is.null(y)) {
+  x <- runif(10)
+  y <- runif(10)
+}
+
+# function body
+# make changes and edits here
+z <- "new object"
+
+return(z)
+
+#return(print('...checking function: fit_regression()'))
+
+} # end of function fit_regression
+# --------------------------------------
+fit_regression()
