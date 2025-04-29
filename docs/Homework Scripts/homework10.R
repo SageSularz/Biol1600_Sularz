802b75e6cff1d413a83b2af85c78c9f07770176b
SageSularz <sage.sularz@gmail.com>
2025-04-29 18:28:24 -0400

    Finish off hmks

--- docs/Homework Scripts/homework10.R
+++ docs/Homework Scripts/homework10.R
@@ -31,13 +31,13 @@ waffle <- ggplot(data=tabled_data2,
   aes(fill = Color, values = Freq)) +
   waffle::geom_waffle(n_rows = 8,
                       size = 0.33,
-                      colour = "white",
-                      title = "color") +
+                      colour = "white") +
+  labs(title="color")+
   scale_fill_manual(name = "Color Code",
                     values = my_cols)+
   coord_equal() +
   theme_void()
-p8
+waffle
 
 # plot 2: tree map showing color dist
 treemap(dtf=tabled_data2,
@@ -62,6 +62,51 @@ p5
 
 
 
+
+
+# Isolate the top five colors in set
+top5 <- tabled_data2 %>%
+  arrange(desc(Freq)) %>%
+  slice(1:5)
+
+# Assign names to color codes
+color_names <- c(
+  "0" = "Black",
+  "70" = "Reddish Brown",
+  "71" = "Light Bluish Gray",
+  "72" = "Dark Bluish Gray",
+  "308" = "Dark Brown"
+)
+
+# Replace code with color name
+top5 <- top5 %>%
+  mutate(Color = recode(as.character(Color), !!!color_names))
+
+# Assign correct hex codes (from LEGO.com)
+my_cols <- c("#05131D", "#582A12", "#A0A5A9", "#6C6E68", "#352100")
+
+
+lolli_colors <- ggplot(data=top5) +
+  aes(x=Color, y=Freq) +
+  geom_segment(aes(x=Color,
+                   xend=Color, y=0,
+                   yend=Freq),
+               color=my_cols,
+               linewidth=2) +
+  geom_point( color=my_cols, size=7) +
+  labs(title="Top Colors",
+       x="",
+       y="Count") +
+  coord_flip() +
+  theme_light(base_size=20,base_family=
+                "Monaco") +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.y = element_blank(),
+    plot.title.position = "plot",
+    plot.title = element_text(hjust = 0))
+lolli_colors
 
 
 
